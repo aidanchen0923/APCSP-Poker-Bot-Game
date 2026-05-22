0541ba6f684666036c56ed72822f48eebe670a05
Aravind <aravind.arul@outlook.com>
2026-05-22 13:05:39 -0700

    started messing around

--- src/asspoker.py
+++ src/asspoker.py
@@ -1,1 +1,14 @@
-#sup this is the file
+import treys 
+from treys import Evaluator
+from treys import Card
+evaluator = Evaluator()
+
+fullDeck = [f'{rank}{suit}' for rank in 'AKQJT98765432' for suit in 'shdc']
+
+holeCards = [(input('Enter Hole Card 1:').strip(' ')).capitalize(), (input('Enter Hole Card 2:').strip(' ')).capitalize()]
+
+hand = [Card.new(holeCards[0]), Card.new(holeCards[1])]
+
+board = []
+
+
